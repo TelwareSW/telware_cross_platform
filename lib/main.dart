4521394bff55224c3af91a6b500131af991f6aa1
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-10-16 02:19:44 +0300

    feat(auth): add signup route

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/theme/auth_theme.dart';
+import 'features/auth/view/screens/sign_up_screen.dart';
 
 void main() {
-  runApp(const TalWare());
+  runApp(const ProviderScope(
+    child: TalWare(),
+  ));
 }
 
 class TalWare extends StatelessWidget {
@@ -10,12 +15,13 @@ class TalWare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TalWare',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
+      theme: authTheme,
+      routes: {
+        SignUpScreen.route: (context) => const SignUpScreen(),
+      },
+      initialRoute: SignUpScreen.route,
     );
   }
 }
