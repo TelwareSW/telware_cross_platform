82f595b047c3ee3bac138f5d202e17d92b6e016d
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-10-15 20:37:50 +0300

    feat(ui): add Palette class for managing colors

---
+++ lib/core/theme/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette{
+   static const Color background = Color(0x001d2733);
+   static const Color primary = Color(0x005FA3DE);
+   static const Color accent = Color(0x006EB2EF);
+   static const Color primaryText = Color(0x00FFFFFF);
+   static const Color subtitleText = Color(0x007D8B99);
+}
