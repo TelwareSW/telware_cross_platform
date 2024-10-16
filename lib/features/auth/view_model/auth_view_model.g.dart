67474bb75f8bf7f55271d0b9cdde2007dc26c6ee
Ahmed Aladdin <aamieeu@gmail.com>
2024-10-16 05:12:15 +0300

    feat(auth): create the authentication view model

---
+++ lib/features/auth/view_model/auth_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authViewModelHash() => r'f0a0df39f859e83657a1cf5aea8d9988d7a976e1';
+
+/// See also [AuthViewModel].
+@ProviderFor(AuthViewModel)
+final authViewModelProvider =
+    NotifierProvider<AuthViewModel, AuthState>.internal(
+  AuthViewModel.new,
+  name: r'authViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthViewModel = Notifier<AuthState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
