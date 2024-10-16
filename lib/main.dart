b96c2f88f4430f6ec9e5f406946b4a8c75a2308d
Ahmed Aladdin <aamieeu@gmail.com>
2024-10-16 05:13:21 +0300

    feat(auth): initialize the authentication process in the TelWare widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:telware_cross_platform/features/auth/view_model/auth_view_model.dart';
 
 void main() {
   runApp(const ProviderScope(child: TelWare()));
 }
 
-class TelWare extends StatelessWidget {
+class TelWare extends ConsumerStatefulWidget {
   const TelWare({super.key});
+
+  @override
+  ConsumerState<TelWare> createState() => _TelWareState();
+}
+
+class _TelWareState extends ConsumerState<TelWare> {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(authViewModelProvider.notifier).init();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
