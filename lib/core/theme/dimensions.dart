43711a09ebacdff70d236c5bcdb56d4e8e81f24d
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-10-17 10:09:09 +0300

    refactor(auth): add extra dimension

--- lib/core/theme/dimensions.dart
+++ lib/core/theme/dimensions.dart
@@ -1,6 +1,7 @@
 class Dimensions {
   static const double inputPaddingLeft = 40;
   static const double inputPaddingRight = 40;
+  static const double inputPaddingBottom = 25;
   static const double inputContentPadding = 14;
   static const double inputBorderRadius = 10;
   static const double inputBorderWidth = 1;
