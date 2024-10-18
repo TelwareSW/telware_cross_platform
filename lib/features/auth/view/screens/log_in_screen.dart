5b4a76b4e5fc36afdc629d2b1fa25981206a0866
Ahmed Aladdin <aamieeu@gmail.com>
2024-10-18 11:35:29 +0300

    refactor(auth-cross): move the social login widget to a separate file

--- lib/features/auth/view/screens/log_in_screen.dart
+++ lib/features/auth/view/screens/log_in_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:telware_cross_platform/core/utils.dart';
 import 'package:telware_cross_platform/core/view/widget/responsive.dart';
 import 'package:telware_cross_platform/features/auth/view/widget/shake_my_auth_input.dart';
+import 'package:telware_cross_platform/features/auth/view/widget/social_log_in.dart';
 import 'package:telware_cross_platform/features/auth/view/widget/title_element.dart';
 import 'package:telware_cross_platform/features/auth/view/widget/circular_button.dart';
 import 'package:telware_cross_platform/core/theme/sizes.dart';
@@ -170,48 +171,4 @@ class LogInScreenState extends State<LogInScreen> {
       ),
     );
   }
-}
-
-class SocialLogIn extends StatelessWidget {
-  const SocialLogIn({
-    super.key,
-  });
-
-  static const double _size = 35;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12),
-          child: Container(
-              height: 1,
-              width: 250,
-              decoration: const BoxDecoration(color: Palette.primaryText)),
-        ),
-        const Text('or Continue with'),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _socialButton(img: 'google-g-white.png', onTap: () {}),
-            _socialButton(img: 'facebook-f-white.png', onTap: () {}),
-            _socialButton(img: 'github-icon-white.png', onTap: () {}),
-          ],
-        )
-      ],
-    );
-  }
-
-  Widget _socialButton({required VoidCallback onTap, required String img}) =>
-      Container(
-        margin: const EdgeInsets.symmetric(horizontal: 18),
-        width: _size,
-        height: _size,
-        child: InkWell(
-          onTap: onTap,
-          child: Image.asset('assets/imgs/$img'),
-        ),
-      );
 }
