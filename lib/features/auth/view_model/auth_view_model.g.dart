d860dc43d197c595e2df56b3d07854ee090ec31a
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-10-18 13:57:39 +0300

    feat(auth): add signUp logic to auth-view-model

--- lib/features/auth/view_model/auth_view_model.g.dart
+++ lib/features/auth/view_model/auth_view_model.g.dart
@@ -6,7 +6,7 @@ part of 'auth_view_model.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$authViewModelHash() => r'f0a0df39f859e83657a1cf5aea8d9988d7a976e1';
+String _$authViewModelHash() => r'688a996c5b194d836b81c33bbb3067a02bde6bd1';
 
 /// See also [AuthViewModel].
 @ProviderFor(AuthViewModel)
