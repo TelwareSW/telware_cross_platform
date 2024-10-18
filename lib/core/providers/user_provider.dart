56a861486d89a087fdb9f70492a8b4dd2f9eed4e
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-10-18 13:53:28 +0300

    feat(auth): create user provider

---
+++ lib/core/providers/user_provider.dart
@@ -0,0 +1,9 @@
+// todo: create the user provider in a well-named file
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:telware_cross_platform/core/models/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) {
+  return null;
+});
