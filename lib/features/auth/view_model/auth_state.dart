13bc832252b7e4a1cb2e8d63145e12f8d6e91a3f
Ahmed Aladdin <aamieeu@gmail.com>
2024-10-19 08:31:26 +0300

    feat(auth): update auth state to a class

--- lib/features/auth/view_model/auth_state.dart
+++ lib/features/auth/view_model/auth_state.dart
@@ -1,6 +1,20 @@
-enum AuthState {
+enum AuthStateType {
   init,
   loading,
+  failed,
   unauthorized,
-  authorized
+  authorized,
+}
+
+class AuthState {
+  final AuthStateType type;
+  final String? message;
+
+  const AuthState._(this.type, [this.message]);
+
+  static const init = AuthState._(AuthStateType.init);
+  static const loading = AuthState._(AuthStateType.loading);
+  static const unauthorized = AuthState._(AuthStateType.unauthorized);
+  static const authorized = AuthState._(AuthStateType.authorized);
+  static AuthState failed(String message) => AuthState._(AuthStateType.failed, message);
 }
