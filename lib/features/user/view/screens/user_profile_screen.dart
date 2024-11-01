d0037bfd62c733cbf833c2680f04d1f79ddf0cd1
Bishoywadea <bishoyw.fathy@gmail.com>
2024-11-01 15:51:50 +0200

    FEAT(profile-pic): add floating action button on user profile screen

--- lib/features/user/view/screens/user_profile_screen.dart
+++ lib/features/user/view/screens/user_profile_screen.dart
@@ -10,6 +10,8 @@ import 'package:telware_cross_platform/features/user/view/widget/settings_option
 import 'package:telware_cross_platform/features/user/view/widget/settings_section.dart';
 import 'package:telware_cross_platform/features/user/repository/user_local_repository.dart';
 
+import '../../../stories/view/screens/add_my_image_screen.dart';
+
 class UserProfileScreen extends ConsumerStatefulWidget {
   static const String route = '/profile';
 
@@ -32,67 +34,89 @@ class _UserProfileScreen extends ConsumerState<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 145.0,
-            toolbarHeight: 80,
-            floating: false,
-            pinned: true,
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () => context.pop(),
-            ),
-            actions: [
-              IconButton(icon: const Icon(Icons.edit),
-                  onPressed: () => context.push(Routes.profileInfo)),
-              const SizedBox(width: 16),
-              const Icon(Icons.more_vert),
-            ],
-            flexibleSpace: LayoutBuilder(
-              builder: (context, constraints) {
-                double factor = _calculateFactor(constraints);
-                return FlexibleSpaceBar(
-                  title: ProfileHeader(factor: factor),
-                  centerTitle: true,
-                  background: Container(
-                    alignment: Alignment.topLeft,
-                    color: Palette.trinary,
-                    padding: EdgeInsets.zero,
-                  ),
-                );
-              },
-            ),
-          ),
-          SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  SettingsSection(
-                    title: "Info",
-                    settingsOptions: const [],
-                    actions: [
-                      SettingsOptionWidget(text: _user.phone ?? "",
-                        icon: null,
-                        subtext: "Mobile",
-                      ),
-                      if (_user.bio != "")
-                        SettingsOptionWidget(icon: null,
-                            text: _user.bio,
-                            subtext: "Bio"
-                        ),
-                      if (_user.username != "")
-                        SettingsOptionWidget(icon: null,
-                            text: "@${_user.username}",
-                            subtext: "Username"
-                        ),
-                    ],
-                  ),
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 145.0,
+                toolbarHeight: 80,
+                floating: false,
+                pinned: true,
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => context.pop(),
+                ),
+                actions: [
+                  IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () => context.push(Routes.profileInfo)),
+                  const SizedBox(width: 16),
+                  const Icon(Icons.more_vert),
                 ],
+                flexibleSpace: LayoutBuilder(
+                  builder: (context, constraints) {
+                    double factor = _calculateFactor(constraints);
+                    return FlexibleSpaceBar(
+                      title: ProfileHeader(factor: factor),
+                      centerTitle: true,
+                      background: Container(
+                        alignment: Alignment.topLeft,
+                        color: Palette.trinary,
+                        padding: EdgeInsets.zero,
+                      ),
+                    );
+                  },
+                ),
               ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    SettingsSection(
+                      title: "Info",
+                      settingsOptions: const [],
+                      actions: [
+                        SettingsOptionWidget(
+                          text: _user.phone ?? "",
+                          icon: null,
+                          subtext: "Mobile",
+                        ),
+                        if (_user.bio != "")
+                          SettingsOptionWidget(
+                              icon: null, text: _user.bio, subtext: "Bio"),
+                        if (_user.username != "")
+                          SettingsOptionWidget(
+                              icon: null,
+                              text: "@${_user.username}",
+                              subtext: "Username"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SliverToBoxAdapter(
+                child: SizedBox(height: Dimensions.sectionGaps),
+              ),
+            ],
           ),
-
-          const SliverToBoxAdapter(
-            child: SizedBox(height: Dimensions.sectionGaps),
+          Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 155.0, right: 16.0),
+              child: FloatingActionButton(
+                child: Icon(Icons.add),
+                backgroundColor: Palette.primary,
+                shape: CircleBorder(),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const AddMyImageScreen(
+                          destination: 'profile'),
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
         ],
       ),
@@ -102,7 +126,8 @@ class _UserProfileScreen extends ConsumerState<UserProfileScreen> {
   double _calculateFactor(BoxConstraints constraints) {
     double maxExtent = 130.0;
     double scrollOffset = constraints.maxHeight - kToolbarHeight;
-    double factor = scrollOffset > 0 ? (maxExtent - scrollOffset) / maxExtent * 90.0 : 60.0;
+    double factor =
+        scrollOffset > 0 ? (maxExtent - scrollOffset) / maxExtent * 90.0 : 60.0;
     return factor.clamp(0, 90.0);
   }
 }
