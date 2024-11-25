872a2f0e5786a03dc1644c552d8dc1e9e0d4ce68
Ahmed Aladdin <aamieeu@gmail.com>
2024-11-25 14:31:33 +0200

    config: auto generated files

---
+++ lib/features/chat/view_model/chats_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chats_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$chatsViewModelHash() => r'17ad9e43168483798eb52682e91a573071021e8b';
+
+/// See also [ChatsViewModel].
+@ProviderFor(ChatsViewModel)
+final chatsViewModelProvider =
+    NotifierProvider<ChatsViewModel, List<ChatModel>>.internal(
+  ChatsViewModel.new,
+  name: r'chatsViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$chatsViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ChatsViewModel = Notifier<List<ChatModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/features/chat/view_model/chatting_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chatting_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authRemoteRepositoryHash() =>
+    r'f4f07165495d4b759ae2396a948b8e3da31759ee';
+
+/// See also [authRemoteRepository].
+@ProviderFor(authRemoteRepository)
+final authRemoteRepositoryProvider = Provider<ChattingController>.internal(
+  authRemoteRepository,
+  name: r'authRemoteRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRemoteRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthRemoteRepositoryRef = ProviderRef<ChattingController>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
