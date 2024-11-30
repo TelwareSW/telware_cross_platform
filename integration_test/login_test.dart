dc5e7702a76c973dd79aa321f854d6569f4872be
eslamwageh <53353517+eslamwageh@users.noreply.github.com>
2024-11-30 15:56:55 +0200

    for mr ahmed alaa

--- integration_test/login_test.dart
+++ integration_test/login_test.dart
@@ -2,112 +2,124 @@ import 'package:telware_cross_platform/core/constants/keys.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:telware_cross_platform/features/home/view/screens/home_screen.dart';
 import 'package:telware_cross_platform/main.dart' as app;
+
+import 'utils/test_users.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Login Tests', () {
-    // setUpAll(() async {
-    //   // Start the app
-    //   app.main();
+  //DONE
+  // group("Login Tests", () {
+  //   testWidgets("valid login credentials", (WidgetTester tester) async {
+  //     app.main();
+  //     await tester.pumpAndSettle(const Duration(seconds: 2));
+  //
+  //     var emailInput = find.byKey(Keys.logInEmailKey);
+  //     var passwordInput = find.byKey(Keys.logInPasswordKey);
+  //     var submitButton = find.byKey(Keys.logInSubmitKey);
+  //
+  //     await tester.enterText(emailInput, validUsers[0][0]);
+  //     await tester.pumpAndSettle(const Duration(seconds: 1));
+  //     await tester.enterText(passwordInput, validUsers[0][1]);
+  //     await tester.pumpAndSettle(const Duration(seconds: 1));
+  //     await tester.tap(submitButton);
+  //     await tester.pumpAndSettle(const Duration(seconds: 2));
+  //
+  //     // Verify that the user is redirected to the home page
+  //     expect(find.text('TelWare'), findsOneWidget);
+  //   });
+
+    //ToddO: till toasts are solved
+    // testWidgets("invalid login credentials", (WidgetTester tester) async {
+    //   var emailInput = find.byKey(Keys.logInEmailKey);
+    //   var passwordInput = find.byKey(Keys.logInPasswordKey);
+    //   var submitButton = find.byKey(Keys.logInSubmitKey);
+    //
+    //   expect(find.text('Invalid email or password'), findsNothing);
+    //   await tester.enterText(emailInput, invalidUsers[0].email);
+    //   await tester.enterText(passwordInput, invalidUsers[0].password);
+    //   await tester.tap(submitButton);
     //   await tester.pumpAndSettle();
+    //
+    //   expect(find.text('Invalid email or password'), findsOneWidget);
+    //   expect(find.text(invalidUsers[0].email), findsOneWidget);
     // });
 
-    testWidgets('valid login credentials', (WidgetTester tester) async {
-      app.main();
-      await tester.pumpAndSettle(const Duration(seconds: 2));
-      // Enter valid email
-      //var emailInput = find.byType(TextFormField).at(0);
-      var emailInput = find.byKey(Keys.logInEmailKey);
-      await tester.enterText(emailInput, 'validuser@example.com');
-      await tester.pumpAndSettle(const Duration(seconds: 1));
-      //await tester.pumpAndSettle(const Duration(seconds: 1));
-      // Enter valid password
-      // await tester.enterText(
-      //   find.byKey(const Key('login_password_input')),
-      //   'validpassword',
-      // );
-      // await tester.pumpAndSettle(const Duration(seconds: 1));
-      // // Tap on login button
-      // await tester.tap(find.byKey(const Key('login_submit_button')));
-      // await tester.pumpAndSettle();
+  //DONE
+    // testWidgets("invalid email or short password", (WidgetTester tester) async {
+    //   app.main();
+    //   await tester.pumpAndSettle(const Duration(seconds: 2));
+    //
+    //   var emailInput = find.byKey(Keys.logInEmailKey);
+    //   var passwordInput = find.byKey(Keys.logInPasswordKey);
+    //   var submitButton = find.byKey(Keys.logInSubmitKey);
+    //
+    //   expect(find.text('Enter a valid email address'), findsNothing);
+    //   expect(find.text('Password must be at least 8 characters long'), findsNothing);
+    //   await tester.enterText(emailInput, validUsers[0][0]);
+    //   await tester.pumpAndSettle(const Duration(seconds: 1));
+    //
+    //   await tester.enterText(passwordInput, validUsers[0][1]);
+    //   await tester.pumpAndSettle(const Duration(seconds: 1));
+    //
+    //   await tester.tap(submitButton);
+    //   await tester.pumpAndSettle(const Duration(seconds: 2));
+    //
+    //   expect(find.text('Enter a valid email address'), findsNothing);
+    //   expect(find.text('Password must be at least 8 characters long'), findsNothing);
+    //
+    // });
 
-      // // Verify navigation to home
-      // expect(find.text('/'), findsOneWidget);
+    // ASK ahmed about shaking
+    testWidgets("blank login credentials", (WidgetTester tester) async {
+      var submitButton = find.byKey(Keys.logInSubmitKey);
+
+      expect(find.text('Email is required'), findsNothing);
+      await tester.tap(submitButton);
+      await tester.pumpAndSettle();
+
+      expect(find.text('Email is required'), findsOneWidget);
+      expect(find.text('Password is required'), findsOneWidget);
     });
 
-    // testWidgets('invalid login credentials', (WidgetTester tester) async {
-    //   // Assert error message is not visible at the beginning
-    //   expect(find.text('Invalid email or password'), findsNothing);
-
-    //   // Enter invalid email and password
-    //   await tester.enterText(
-    //     find.byKey(const Key('login-email-input')),
-    //     'invaliduser@example.com',
-    //   );
-    //   await tester.enterText(
-    //     find.byKey(const Key('login-password-input')),
-    //     'wrongpassword',
-    //   );
-    //   // Tap on login button
-    //   await tester.tap(find.byKey(const Key('login_submit_button')));
-    //   await tester.pumpAndSettle();
-
-    //   // Assert error message is shown
-    //   expect(find.text('Invalid email or password'), findsOneWidget);
-
-    //   // Assert that the email is still in the input field
-    //   expect(find.text('invaliduser@example.com'), findsOneWidget);
+    // DONE
+    // testWidgets("forget password & signup click", (WidgetTester tester) async {
+    //   app.main();
+    //   await tester.pumpAndSettle(const Duration(seconds: 2));
+    //
+    //   var signupLink = find.byKey(Keys.logInSignUpKey);
+    //   await tester.tap(signupLink);
+    //   await tester.pumpAndSettle(const Duration(seconds: 1));
+    //
+    //   expect(find.text('Your email address'), findsOneWidget);
+    //   // navigate back to test the forget password
+    //   await tester.tap(find.byTooltip('Back'));
+    //   await tester.pumpAndSettle(const Duration(seconds: 1));
+    //
+    //   expect(find.text('Enter your credentials'), findsOneWidget);
     // });
 
-    // testWidgets('blank login credentials', (WidgetTester tester) async {
-    //   // Assert no validation message at the beginning
-    //   expect(find.text('Email is required'), findsNothing);
-    //   expect(find.text('Password is required'), findsNothing);
-
-    //   // Tap on login button without filling form
-    //   await tester.tap(find.byKey(const Key('login_submit_button')));
-    //   await tester.pumpAndSettle();
-
-    //   // Assert validation messages
-    //   expect(find.text('Email is required'), findsOneWidget);
-    //   expect(find.text('Password is required'), findsOneWidget);
-    // });
-
-    // testWidgets('forgot password, signup and long input tests',
-    //     (WidgetTester tester) async {
-    //   // Assert 'reset your password' message is not visible initially
-    //   expect(find.text('Reset your password'), findsNothing);
-
-    //   // Tap on forgot password
-    //   await tester.tap(find.byKey(const Key('login_forgot_password_button')));
-    //   await tester.pumpAndSettle();
-
-    //   // Assert 'reset your password' message is visible
-    //   expect(find.text('Reset your password'), findsOneWidget);
-
-    //   // Close modal
-    //   await tester.tap(find.byKey(const Key('modal-close-button')));
-    //   await tester.pumpAndSettle();
-
-    //   // Assert 'reset your password' message is no longer visible
-    //   expect(find.text('Reset your password'), findsNothing);
-
-    //   // Long input test
-    //   await tester.enterText(
-    //     find.byKey(const Key('login-email-input')),
-    //     'averylongemailaddress@averylongdomainname.com',
-    //   );
-    //   await tester.enterText(
-    //     find.byKey(const Key('login-password-input')),
-    //     'averylongpasswordthatexceedslimits',
-    //   );
-    //   await tester.tap(find.byKey(const Key('login_submit_button')));
-    //   await tester.pumpAndSettle();
-
-    //   // Assert error message for long input
-    //   expect(find.text('Very long email or password'), findsOneWidget);
-    // });
-  });
+  //DONE
+  //   testWidgets("long inputs tests", (WidgetTester tester) async {
+  //       app.main();
+  //       await tester.pumpAndSettle(const Duration(seconds: 2));
+  //
+  //     var emailInput = find.byKey(Keys.logInEmailKey);
+  //     var passwordInput = find.byKey(Keys.logInPasswordKey);
+  //     var submitButton = find.byKey(Keys.logInSubmitKey);
+  //
+  //     await tester.enterText(emailInput, invalidUsers[2][0]);
+  //     await tester.pumpAndSettle(const Duration(seconds: 1));
+  //
+  //     await tester.enterText(passwordInput, invalidUsers[2][1]);
+  //     await tester.pumpAndSettle(const Duration(seconds: 1));
+  //
+  //     await tester.tap(submitButton);
+  //     await tester.pumpAndSettle();
+  //
+  //     expect(find.text('Very long email or password'), findsOneWidget);
+  //   });
+  //});
 }
