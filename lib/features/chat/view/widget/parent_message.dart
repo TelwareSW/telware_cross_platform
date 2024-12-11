35d74e54254f94f95e23d8d52a3afccf309b1d11
Ahmed Aladdin <aamieeu@gmail.com>
2024-12-11 15:59:52 +0200

    fix: very tall reply message

--- lib/features/chat/view/widget/parent_message.dart
+++ lib/features/chat/view/widget/parent_message.dart
@@ -8,41 +8,36 @@ class ParentMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxHeight: 100.0, // Set your desired maximum height here
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.15),
+        borderRadius: BorderRadius.circular(6),
+        border: const Border(
+          left: BorderSide(color: Colors.white, width: 4.0), // White edge
+        ),
+        shape: BoxShape.rectangle,
       ),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.15),
-          borderRadius: BorderRadius.circular(6),
-          border: const Border(
-            left: BorderSide(color: Colors.white, width: 4.0), // White edge
+      // height: 50,
+      margin: const EdgeInsets.symmetric(vertical: 3),
+      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SenderNameWidget(
+            DateTime.now().millisecondsSinceEpoch,
+            Colors.white,
+            showInfo: true,
+            isSentByMe: false,
+            userId: parentMessage?.senderId ?? '',
           ),
-          shape: BoxShape.rectangle,
-        ),
-        // height: 50,
-        margin: const EdgeInsets.symmetric(vertical: 3),
-        padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SenderNameWidget(
-              DateTime.now().millisecondsSinceEpoch,
-              Colors.white,
-              showInfo: true,
-              isSentByMe: false,
-              userId: parentMessage?.senderId ?? '',
-            ),
-            Text(
-              parentMessage?.content!.getContent() ?? '',
-              style: const TextStyle(color: Colors.white),
-              overflow: TextOverflow
-                  .ellipsis, // Trim the text if it exceeds the available space
-              maxLines: 3,
-            )
-          ],
-        ),
+          Text(
+            parentMessage?.content!.getContent() ?? '',
+            style: const TextStyle(color: Colors.white),
+            overflow: TextOverflow
+                .ellipsis, // Trim the text if it exceeds the available space
+            maxLines: 3,
+          )
+        ],
       ),
     );
   }
